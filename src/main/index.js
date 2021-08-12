529fb83b7b939d8d18f838026d34d914917a18f8
理想三旬 <83727128@qq.com>
2021-08-13 03:07:33 +0800

    新增功能只允许应用单例

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const { app } = require("electron");
-const { createWindow } = require("./window/main");
+const { createWindow, getMainWindow } = require("./window/main");
 const { createUpdateWindow } = require("./window/update");
 const { checkForUpdates } = require("./update");
 const handleIPC = require("./ipc");
@@ -12,24 +12,43 @@ require("./cmd");
 app.commandLine.appendSwitch("--ignore-certificate-errors", "true");
 app.commandLine.appendSwitch("--disable-web-security", "true");
 
-app.on("ready", () => {
-  log.info("App ready...");
-  checkForUpdates().then((info) => {
-    if (info) {
-      //开启更新进程
-      return createUpdateWindow();
+/**
+ * 如果你的程序是应用的主要实例并且当这个方法返回true时，你应该继续让你的程序运行。
+ * 如果当它返回false如果你的程序没有取得锁，它应该立刻退出，并且将参数发送给那个已经取到锁的进程。
+ * 保持应用一直处于单例
+ */
+const gotTheLock = app.requestSingleInstanceLock();
+if (!gotTheLock) {
+  app.quit();
+} else {
+  //当第二个实例被执行并且调用 app.requestSingleInstanceLock() 时，这个事件将在你的应用程序的首个实例中触发
+  app.on("second-instance", () => {
+    let mainWindow = getMainWindow();
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
     }
-    createWindow();
-    handleIPC();
   });
-});
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
+  app.on("ready", () => {
+    log.info("App ready...");
+    checkForUpdates().then((info) => {
+      if (info) {
+        //开启更新进程
+        return createUpdateWindow();
+      }
+      createWindow();
+      handleIPC();
+    });
+  });
 
-app.on("activate", () => {
-  createWindow();
-});
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
+
+  app.on("activate", () => {
+    createWindow();
+  });
+}
