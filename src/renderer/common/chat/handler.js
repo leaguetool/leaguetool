8dc3e95efe752a4ddab41adaf4591642c0a3d559
good-ideal <83727128@qq.com>
2023-08-02 21:25:55 +0800

    refactor: ♻️ chat重构完成

--- src/renderer/common/chat/handler.js
+++ src/renderer/common/chat/handler.js
@@ -31,8 +31,9 @@ let Handler = function () {
 
     switch (msgBody.type) {
       case "CHAT": {
-        mitt.emit("send-message-checkbottom", {});
-        store.dispatch("chat/addMessage", JSON.parse(msgBody.data));
+        store.dispatch("chat/addMessage", JSON.parse(msgBody.data)).then(() => {
+          mitt.emit("send-message-checkbottom", {});
+        });
         break;
       }
       //其他的消息
--- src/renderer/views/Game/Chat/Chat.vue
+++ src/renderer/views/Game/Chat/Chat.vue
@@ -4,14 +4,13 @@
       v-show="!!chatList.length"
       ref="vsl"
       class="scroller"
-      :start="chatList.value.length > 0 ? chatList.value.length - 1 : 0"
+      :start="start"
       :class="{ 'chat-body-adaptation': connectStatus }"
       :data-key="'id'"
       :data-sources="chatList"
       :data-component="MessageItem"
       :estimate-size="100"
       @tobottom="onBottom"
-      @resized="onItemRendered"
     >
     </VirtualList>
     <a-empty
@@ -39,26 +38,14 @@ export default {
   },
   components: { MessageItem, MessageTips, VirtualList },
   setup: () => {
-    const vsl = ref();
+    const vsl = ref(null);
     const store = useStore();
     const chatList = computed(() => {
       return store.state.chat.chatList;
     });
-
-    const startIndex = computed(() => {
-      console.log("computedstartIndex: ", chatList.value.length);
-      return chatList.value.length > 0 ? chatList.value.length - 1 : 0;
-    });
-
-    console.log("startIndex: ", startIndex.value);
-
-    const isBottom = computed(() => {
-      return (
-        vsl.value.getScrollSize() -
-          (vsl.value.getOffset() + vsl.value.getClientSize()) <=
-        10
-      );
-    });
+    const start = ref(
+      chatList.value.length > 0 ? chatList.value.length - 1 : 0
+    );
 
     //创建一个节流函数
     const throttle = (fn, delay) => {
@@ -74,54 +61,50 @@ export default {
         }
       };
     };
-    //触底事件
+
+    //触底事件 200ms内只触发一次 用于清零底部新消息数量统计
     const onBottom = throttle(() => {
       store.dispatch("chat/setMessageTipsCount", 0);
-    }, 500);
+    }, 200);
 
-    const onItemRendered = () => {
-      if (!vsl.value) {
-        return;
-      }
-
-      //当有新消息来了,如果当前在底部就让继续滚动到底部,否则滚动不会变化
-      // if (
-      //   vsl.value.getScrollSize() -
-      //     (vsl.value.getOffset() + vsl.value.getClientSize()) <
-      //   10
-      // ) {
-      //   nextTick(() => {
-      //     setVirtualListToBottom();
-      //   });
-      // }
-    };
-
-    //滚动到底部
+    //手动滚动到底部 并且把底部新消息数量统计清零
     const setVirtualListToBottom = () => {
       if (vsl.value) {
-        // vsl.value.scrollToBottom();
+        vsl.value.scrollToBottom();
         //滚动到底部那么就需要把底部新消息数量统计清零
         store.dispatch("chat/setMessageTipsCount", 0);
       }
     };
+
+    //自己发送消息时触发
     mitt.on("send-message-end", () => {
       setVirtualListToBottom();
     });
+
+    //监听消息列表变化 当收到CHAT_ADD事件时触发
     mitt.on("send-message-checkbottom", () => {
-      console.log("是否在底部: ", !isBottom.value);
-      if (!isBottom.value) {
-        //如果不在底部,那么就需要把底部新消息数量统计加一 内部使用了count++
-        store.dispatch("chat/setMessageTipsCount");
+      if (vsl.value) {
+        const isBottom =
+          vsl.value.getScrollSize() -
+            (vsl.value.getOffset() + vsl.value.getClientSize()) <=
+          100;
+        if (isBottom) {
+          setVirtualListToBottom();
+        } else {
+          //如果不在底部,那么就需要把底部新消息数量统计加一 内部使用了count++
+          store.dispatch("chat/setMessageTipsCount").then(() => {
+            console.log("只增count成功");
+          });
+        }
       }
     });
 
     return {
       chatList,
-      startIndex,
+      start,
       vsl,
       MessageItem: MessageItem,
       setVirtualListToBottom,
-      onItemRendered,
       onBottom,
       simpleImage: Empty.PRESENTED_IMAGE_DEFAULT,
     };
